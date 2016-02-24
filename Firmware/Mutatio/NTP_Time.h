f0eb495580e98ccb5b8a24bb0990ed8a8c39522a
DedeHai <daedae@gmx.ch>
2016-02-24 21:21:50 +0100

    bugfix: FCPU error computation error during RTC updat ;ASD:Q	WçQEç&467#

--- Firmware/Mutatio/NTP_Time.h
+++ Firmware/Mutatio/NTP_Time.h
@@ -1,5 +1,5 @@
 
-#define REQUESTSTOAVERAGE 50 //number of requests to send to server to get the average time during fastupdate
+#define REQUESTSTOAVERAGE 200 //number of requests to send to server to get the average time during fastupdate
 #define ALLOWEDROUNDTRIPDELAY 600 //maximum allowed ping for the NTP server (default: 30) Absolute time accuracy depends on this value
 #define USE_RTC_FOR_CPUSYNC 1 //uncomment this define to use RTC instead of NTP to sync FCPU, use if NTP sync is inaccurate (i.e. more than 2ms fluctuation)
 
@@ -158,6 +158,11 @@ void syncFCPU(void)
   static uint32_t synctime;
   if (syncinprogress == 0)
   {
+    if (RTCsynctime > 48) //update RTC from NTP localtime once per day
+    {
+      updateRTCfromlocaltime();
+      RTCsynctime = 0;
+    }
     //start new sync, save current time:
     Serial.println(F("FCPU timesync started"));
 #ifdef USE_RTC_FOR_CPUSYNC //use RTC
@@ -190,7 +195,7 @@ void syncFCPU(void)
   {
     if (millis() - synctime > 1800000) //check FCPU sync after 30 minutes
     {
-      if (FCPU_timestamp.millistimestamp < millis()) //check if millis overflowed, start a new sync if it did (timestamps are not valid)
+      if (FCPU_timestamp.millistimestamp < (millis() + 2000)) //check if millis overflowed, start a new sync if it did (timestamps are not valid)
       {
         //check deviation of millis() counter to absolute time
         timeStruct nowtimestamp;
@@ -248,16 +253,12 @@ void syncFCPU(void)
           Serial.println(timedifference); //!!!debug
         }
         syncinprogress = 0; //done, start a new sync
+
       }
       else syncinprogress = 0; //error, start a new sync
     }
   }
 
-  if (RTCsynctime > 10) //update RTC from localtime every few hours
-  {
-    updateRTCfromlocaltime();
-    RTCsynctime = 0;
-  }
 
 
 }
@@ -375,7 +376,7 @@ void timeManager(uint8_t forceTimeSync)
           }
         }
         // Serial.println(newoffset);
-        localtimeoffset = ((float)newoffset *  0.2) + (localtimeoffset  * ((float)1.0 - 0.2));
+        localtimeoffset = ((float)newoffset *  0.05) + (localtimeoffset  * ((float)1.0 - 0.05));
         Serial.print("Local Time offset [ms] = ");
         Serial.print(localtimeoffset, 2);
         Serial.println("\t(" + String(newoffset) + ")");
